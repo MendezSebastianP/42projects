1e17ee63448868a40f64099f41eb08c7768b8eab
SebasMendezp <mendezpsebastian@gmail.com>
2024-10-17 13:04:03 +0200

    first changes

---
+++ libft/ft_isalpha.c
@@ -0,0 +1,19 @@
+int ft_isalpha(int c)
+{
+    if ((c > 64 & c < 91) || (c > 96 & c < 123))
+        return(1);
+    else
+        return (0);
+    
+}
+
+#include <ctype.h>
+#include <stdio.h>
+
+int main(void)
+{
+    char a = 'V';
+    printf("%d \n", isalpha(a));
+    printf("%d", ft_isalpha(a));
+    return (0);
+}
