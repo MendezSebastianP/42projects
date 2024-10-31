b0f1cbcb9864e2a4b09482f344f565dfc2c3f9f4
SebasMendezp <mendezpsebastian@gmail.com>
2024-10-31 18:08:47 +0100

    working in calloc

---
+++ libft/ft_calloc.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+void    *calloc(size_t nitems, size_t size)
+{
+        int     i;
+
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int     main(int argc, char *argv[])
+{
+        (void)argc;
+        printf("Our result     : %d \n", ft_atoi(argv[1]));
+        printf("Intended result: %d", atoi(argv[1]));
+        return(0);
+}
+/* int     main(int argc, char *argv[])
+{
+        (void)argc;
+        printf("Our result     : %d \n", ft_atoi(" \n \t -+2147483647asd"));
+        printf("Intended result: %d", atoi(" \n \t +-2147483647asd"));
+        return(0);
+}  */
+
+
