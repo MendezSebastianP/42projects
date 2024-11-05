e0e490d8f994074f88bf68cd6a588ee3b5688fac
Sebastian Mendez-Pineda <smendez-@paul-f5Ar3s2.clusters.42paris.fr>
2024-11-05 13:15:14 +0100

    working in itoa

--- libft/sup/ft_itoa.c
+++ libft/sup/ft_itoa.c
@@ -1,15 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   itoa.c                                             :+:      :+:    :+:   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: smendez- <smendez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 09:42:49 by smendez-          #+#    #+#             */
-/*   Updated: 2024/11/05 10:50:05 by smendez-         ###   ########.fr       */
+/*   Updated: 2024/11/05 13:15:04 by smendez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/* il y a un problemen avec les longs int, je peux pas gerer le min,
+meme dans les autres fonctions... */
 #include <stdlib.h>
 
 int	powerf(int b)
@@ -29,6 +31,11 @@ int sizenbr(int numb)
     int i;
 
     i = 1;
+    if (numb < 0)
+    {
+        i++;
+        numb = numb * -1;
+    }
     if ((numb / 10) == 0)
         return (1);
     while ((numb / 10) > 0)
@@ -39,23 +46,48 @@ int sizenbr(int numb)
     return (i);
 }
 
-char    *ft_itoa(int n)
+char    *ft_itoa(int k)
 {
     int i;
-    int j;
+    long int j;
+    long int n;
     int sizen;
     char    *str1;
 
+    n = k;
     i = 0;
-    j = n;
     sizen = sizenbr(n);
-    str1 = malloc(sizen * sizeof(int));
+    str1 = malloc(sizen * sizeof(long int));
     if (!str1)
         return (NULL);
-    while(j > 10)
-        j = j / 10;
-    str1[i] = j + 48;
-    j = j * powerf(sizen - i);
+    if (n < 0)
+    {
+        str1[i] = '-';
+        i++;
+        n = n * -1;
+    }
+    j = n;
+    while(i < sizen)
+    {
+        while(j > 9)
+            j = j / 10;
+        str1[i] = j + 48;
+        
+        n = n - (j * powerf(sizen - i - 1));
+        j = n;
+        i++;
+    }
+    str1[i] = '\0';
     return(str1);
+}
+#include <stdlib.h>
+#include <stdio.h>
+int main(int c, char *v[])
+{
+    (void)c;
+    char    *s1 = ft_itoa(atoi(v[1]));
+    printf("Our function: %s", s1);
+    free(s1);
+    return (0);
 }
 
