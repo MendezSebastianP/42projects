651c5d0713475e97359229cfe1ee949d8e780e8a
Sebastian Mendez-Pineda <mendezpsebastian@gmail.com>
2024-12-22 17:09:25 +0100

    adding pipex

---
+++ pipex/tutorial_files/access.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+
+/* int main(int argc, const char *argv[]) 
+{
+    int fd = access("sample.txt", F_OK);
+    if (fd == -1) 
+    {
+        printf("Error Number: %d\n", errno);
+        perror("Error Description:");
+    } 
+    else 
+    {
+        printf("No error\n");
+    }
+    return 0;
+} */
+
