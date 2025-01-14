fb38a94b2c4a40a20486e7c4cff81e422102f462
Sebastian Mendez-Pineda <mendezpsebastian@gmail.com>
2025-01-14 16:37:21 +0100

    pipex right size, pt2 bonus remaining

--- pipex/pipex_bonus copy.c
+++ pipex/pipex_bonus copy.c
@@ -6,34 +6,14 @@
 /*   By: smendez- <smendez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/23 12:41:53 by smendez-          #+#    #+#             */
-/*   Updated: 2025/01/14 16:37:00 by smendez-         ###   ########.fr       */
+/*   Updated: 2025/01/14 16:07:50 by smendez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	pid4(int **fd1, char *argv[], char **paths, int i)
-{
-	char **temp2;
-	int open_fd;
-	int *fd;
 
-	fd = fd1[0];	
-	open_fd = open(argv[i + 1], O_RDONLY);
-	if (dup2(open_fd, STDIN_FILENO) == -1)
-		(perror("dup2"), exit(EXIT_FAILURE));
-	close(open_fd);
-	if (dup2(fd[1], STDOUT_FILENO) == -1)
-		(perror("dup2"), exit(EXIT_FAILURE));
-	ft_close_all(fd1);
-	temp2 = ft_split(argv[i + 2], ' ');
-	execve(get_path_command(paths, no_args_cmd(argv[i + 2])), temp2, NULL);
-	perror("execve");
-	free(temp2);
-	exit(EXIT_FAILURE);
-}
-
-int	pid_pipe(int **fd1, char *argv[], char **paths, int i)
+void	pid_pipe(int **fd1, char *argv[], char **paths, int i)
 {
 	char **temp2;
 	int *fd;
@@ -57,19 +37,27 @@ int	pid_pipe(int **fd1, char *argv[], char **paths, int i)
 	exit(EXIT_FAILURE);
 }
 
-int multi_pipex(int argc, char *argv[], char **paths, int **fd)
+int	main(int argc, char *argv[], char *envp[])
 {
+	int **fd;
 	int pid[argc - 2];
+	char **paths;
 	int i;
+	int j;
 
 	i = 0;
+	j = 0;
+	if (ft_isequalstr(argv[1], "here_doc"))
+		j = 1;
+	fd = NULL;
 	fd = ft_add_fd(fd, i);
+	paths = get_path(envp);
 	if (pipe(fd[i]) == -1)
 		return (perror("pipe1"),1);
 	pid[i] = fork();
 	if (pid[i] == 0) 
-		pid0(fd, argv, paths, i);
-	while(i < argc - 5)
+		pid0(fd, argv, paths, i + j);
+	while(i + j < argc - 5)
 	{
 		i++;
 		fd = ft_add_fd(fd, i);
@@ -77,52 +65,16 @@ int multi_pipex(int argc, char *argv[], char **paths, int **fd)
 			return (perror("pipe1"),1);
 		pid[i] = fork();
 		if (pid[i] == 0) 
-			pid_pipe(fd, argv, paths, i);
+			pid_pipe(fd, argv, paths, i + j);
 	}
 	pid[i + 1] = fork();
 	if (pid[i + 1] == 0) 
 		pid1(fd, argv, paths, argc - 1);
-	(ft_close_all(fd), wait_all(pid, i + 1), cleanexit(paths));
-	return (cleanexit2(fd), 0);
-}
-
-int	heredoc(int argc, char *argv[], char *envp[], int **fd)
-{
-	int pid[argc - 2];
-	char **paths;
-	int i;
-
-	i = 0;
-	fd = ft_add_fd(fd, i);
-	paths = get_path(envp);
-	if (pipe(fd[i]) == -1)
-		return (perror("pipe1"),1);
-	pid[i] = fork();
-	if (pid[i] == 0) 
-		pid0(fd, argv, paths, i + 1);
-	pid[i + 1] = fork();
-	if (pid[i + 1] == 0) 
-		pid1(fd, argv, paths, argc - 1);
-	(ft_close_all(fd), wait_all(pid, i + 1), cleanexit(paths));
-	return (cleanexit2(fd), 0);
-}
-
-int	main(int argc, char *argv[], char *envp[])
-{
-	char **paths;
-	int **fd;
-	
-	fd = NULL;
-	paths = get_path(envp);
-	if (ft_isequalstr(argv[1], "here_doc"))
-	{
-		heredoc(argc, argv, envp, fd);
-		cleanexit(paths);
-		return (0);
-	}
-	multi_pipex(argc, argv, paths, fd);
+	ft_close_all(fd);
+	wait_all(pid, i + 1);
+	cleanexit(paths);
+	cleanexit2(fd);
 	return (0);
-	
 }
 
 // ./a.out test1.txt cat wc out.txt qui equivaut a
