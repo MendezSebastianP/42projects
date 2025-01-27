18db92d5bf5ddf8ef983c3d051437bcc63c7312a
Sebastian Mendez-Pineda <mendezpsebastian@gmail.com>
2025-01-27 17:39:01 +0100

    so_long 70%

--- so_long/includes/so_long.h
+++ so_long/includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: smendez- <smendez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 13:30:42 by smendez-          #+#    #+#             */
-/*   Updated: 2025/01/21 18:40:46 by smendez-         ###   ########.fr       */
+/*   Updated: 2025/01/27 17:03:16 by smendez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,34 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
+
+#define TILE_SIZE 64
+
+#define KEY_ESC 65307
+#define KEY_W 119
+#define KEY_A 97
+#define KEY_S 115
+#define KEY_D 100
+
+typedef struct s_game
+{
+    void    *mlx_ptr;
+    void    *win_ptr;
+    char    **map_data;
+    int     map_width;
+    int     map_height;
+    void    *grass_img;
+    void    *wall_img;
+    void    *macron_img;
+    void    *money_img;
+    void    *exit_img;
+    void    *win_img;
+    int     px;
+    int     py;
+    int     coins;
+    int     moves;
+    int     ended;
+}   t_game;
 
 typedef struct s_list
 {
--- so_long/so_long.c
+++ so_long/so_long.c
@@ -6,87 +6,263 @@
 /*   By: smendez- <smendez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 17:59:30 by smendez-          #+#    #+#             */
-/*   Updated: 2025/01/23 13:38:43 by smendez-         ###   ########.fr       */
+/*   Updated: 2025/01/27 17:38:30 by smendez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/so_long.h"
 
-void get_map_dimensions(const char *file_path, int *width, int *height)
+static size_t sl_strlen(const char *s)
 {
-    FILE *file = fopen(file_path, "r");
-    if (!file)
-    {
-        perror("Error opening map file");
-        exit(EXIT_FAILURE);
-    }
+    size_t i = 0;
+    while (s && s[i])
+        i++;
+    return i;
+}
 
-    char ch;
-    *width = 0;
-    *height = 0;
-    int current_width = 0;
+static void error_exit(const char *msg)
+{
+    write(2, "Error\n", 6);
+    if (msg)
+        write(2, msg, sl_strlen(msg));
+    write(2, "\n", 1);
+    exit(EXIT_FAILURE);
+}
 
-    while ((ch = fgetc(file)) != EOF)
+static void read_map(const char *filepath, t_game *g)
+{
+    int     fd = open(filepath, O_RDONLY);
+    char    *line;
+    char    **tmp;
+    int     i;
+
+    if (fd < 0)
+        error_exit("Failed to open map file");
+    g->map_height = 0;
+    g->map_data = NULL;
+    g->coins = 0;
+    while ((line = get_next_line(fd)))
     {
-        if (ch == '\n')
+        size_t len = sl_strlen(line);
+        if (len > 0 && line[len - 1] == '\n')
+            line[len - 1] = '\0';
+        if (g->map_height == 0)
+            g->map_width = (int)sl_strlen(line);
+        tmp = g->map_data;
+        g->map_data = malloc(sizeof(char *) * (g->map_height + 2));
+        if (!g->map_data)
+            error_exit("Memory allocation failed");
+        i = 0;
+        while (i < g->map_height)
         {
-            if (*width == 0)
-                *width = current_width;
-            else if (current_width != *width)
+            g->map_data[i] = tmp[i];
+            i++;
+        }
+        g->map_data[i] = line;
+        g->map_data[i + 1] = NULL;
+        free(tmp);
+        {
+            int j = 0;
+            while (line[j])
             {
-                fprintf(stderr, "Error: Inconsistent line lengths in map file.\n");
-                exit(EXIT_FAILURE);
+                if (line[j] == 'C')
+                    g->coins++;
+                j++;
             }
-            current_width = 0;
-            (*height)++;
         }
-        else
-        {
-            current_width++;
-        }
+        g->map_height++;
     }
-    fclose(file);
+    close(fd);
+    if (g->map_height == 0 || g->map_width == 0)
+        error_exit("Empty or invalid map file");
 }
 
-int main(void)
+static void find_player(t_game *g)
 {
-    void *mlx;
-    void *win;
-    void *img;
-    int img_width;
-    int img_height;
-    int map_width;
-    int map_height;
-
-    mlx = mlx_init();
-    if (!mlx)
+    int y = 0;
+    while (y < g->map_height)
     {
-        fprintf(stderr, "Error initializing MLX.\n");
-        return EXIT_FAILURE;
-    }
-    get_map_dimensions("maps/map7.ber", &map_width, &map_height);
-
-    img = mlx_xpm_file_to_image(mlx, "textures/grass.xpm", &img_width, &img_height);
-    if (!img)
-    {
-        fprintf(stderr, "Error loading texture.\n");
-        return EXIT_FAILURE;
-    }
-    win = mlx_new_window(mlx, map_width * img_width, map_height * img_height, "so_long");
-    if (!win)
-    {
-        fprintf(stderr, "Error creating window.\n");
-        return EXIT_FAILURE;
-    }
-    for (int y = 0; y < map_height; y++)
-    {
-        for (int x = 0; x < map_width; x++)
+        int x = 0;
+        while (x < g->map_width)
         {
-            mlx_put_image_to_window(mlx, win, img, x * img_width, y * img_height);
+            if (g->map_data[y][x] == 'P')
+            {
+                g->px = x;
+                g->py = y;
+                g->map_data[y][x] = '0';
+                return;
+            }
+            x++;
         }
+        y++;
     }
-    mlx_loop(mlx);
-    return EXIT_SUCCESS;
 }
 
+static void load_textures(t_game *g)
+{
+    int w, h;
+
+    g->grass_img = mlx_xpm_file_to_image(g->mlx_ptr, "textures/grass.xpm", &w, &h);
+    if (!g->grass_img)
+        error_exit("Failed to load grass.xpm");
+    g->wall_img = mlx_xpm_file_to_image(g->mlx_ptr, "textures/wall.xpm", &w, &h);
+    if (!g->wall_img)
+        error_exit("Failed to load wall.xpm");
+    g->macron_img = mlx_xpm_file_to_image(g->mlx_ptr, "textures/macron.xpm", &w, &h);
+    if (!g->macron_img)
+        error_exit("Failed to load macron.xpm");
+    g->money_img = mlx_xpm_file_to_image(g->mlx_ptr, "textures/money.xpm", &w, &h);
+    if (!g->money_img)
+        error_exit("Failed to load money.xpm");
+    g->exit_img = mlx_xpm_file_to_image(g->mlx_ptr, "textures/exit.xpm", &w, &h);
+    if (!g->exit_img)
+        error_exit("Failed to load exit.xpm");
+    g->win_img = mlx_xpm_file_to_image(g->mlx_ptr, "textures/win.xpm", &w, &h);
+    if (!g->win_img)
+        error_exit("Failed to load win.xpm");
+        
+}
+
+static void draw_tile(t_game *g, char c, int x, int y)
+{
+    mlx_put_image_to_window(g->mlx_ptr, g->win_ptr,
+        g->grass_img, x * TILE_SIZE, y * TILE_SIZE);
+    if (c == '1')
+        mlx_put_image_to_window(g->mlx_ptr, g->win_ptr,
+            g->wall_img, x * TILE_SIZE, y * TILE_SIZE);
+    else if (c == 'C')
+        mlx_put_image_to_window(g->mlx_ptr, g->win_ptr,
+            g->money_img, x * TILE_SIZE, y * TILE_SIZE);
+    else if (c == 'E')
+    {
+        if (g->coins == 0)
+            mlx_put_image_to_window(g->mlx_ptr, g->win_ptr,
+                g->exit_img, x * TILE_SIZE, y * TILE_SIZE);
+        else
+            ;
+    }
+}
+
+static void draw_map(t_game *g)
+{
+    int y = 0;
+    while (y < g->map_height)
+    {
+        int x = 0;
+        while (x < g->map_width)
+        {
+            draw_tile(g, g->map_data[y][x], x, y);
+            x++;
+        }
+        y++;
+    }
+}
+
+static void draw_player(t_game *g)
+{
+    mlx_put_image_to_window(g->mlx_ptr, g->win_ptr,
+        g->macron_img, g->px * TILE_SIZE, g->py * TILE_SIZE);
+}
+
+static void draw_win_message(t_game *g)
+{
+    int win_img_width;
+    int win_img_height;
+
+    win_img_width = 256;
+    win_img_height = 64;
+    int center_x = (g->map_width * TILE_SIZE - win_img_width) / 2;
+    int center_y = (g->map_height * TILE_SIZE - win_img_height) / 2;
+
+    mlx_put_image_to_window(g->mlx_ptr, g->win_ptr, g->win_img, center_x, center_y);
+}
+
+static void render(t_game *g)
+{
+    mlx_clear_window(g->mlx_ptr, g->win_ptr);
+    draw_map(g);
+    draw_player(g);
+    if (g->ended)
+        draw_win_message(g);
+}
+
+static int close_win(t_game *g)
+{
+    (void)g;
+    exit(0);
+    return (0);
+}
+
+static void move_player(t_game *g, int nx, int ny)
+{
+    if (g->ended)
+        return;
+    if (nx < 0 || ny < 0 || ny >= g->map_height || nx >= g->map_width)
+        return;
+    if (g->map_data[ny][nx] == '1')
+        return;
+    if (g->map_data[ny][nx] == 'C')
+    {
+        g->coins--;
+        g->map_data[ny][nx] = '0';
+    }
+    if (g->map_data[ny][nx] == 'E' && g->coins == 0)
+    {
+        g->px = nx;
+        g->py = ny;
+        g->moves++;
+        printf("Moves: %d\n", g->moves);
+        g->ended = 1;
+        render(g);
+        return;
+    }
+    if (g->map_data[ny][nx] == 'E' && g->coins > 0)
+        return;
+    g->px = nx;
+    g->py = ny;
+    g->moves++;
+    printf("Moves: %d\n", g->moves);
+}
+
+static int key_hook(int keycode, t_game *g)
+{
+    if (keycode == KEY_ESC)
+        close_win(g);
+    else if (keycode == KEY_W)
+        move_player(g, g->px, g->py - 1);
+    else if (keycode == KEY_S)
+        move_player(g, g->px, g->py + 1);
+    else if (keycode == KEY_A)
+        move_player(g, g->px - 1, g->py);
+    else if (keycode == KEY_D)
+        move_player(g, g->px + 1, g->py);
+    render(g);
+    return (0);
+}
+
+int main(int argc, char **argv)
+{
+    t_game g;
+
+    if (argc != 2)
+        error_exit("Usage: ./so_long <map.ber>");
+    g.moves = 0;
+    g.coins = 0;
+    g.ended = 0;
+    read_map(argv[1], &g);
+    g.mlx_ptr = mlx_init();
+    if (!g.mlx_ptr)
+        error_exit("Failed to init MiniLibX");
+    g.win_ptr = mlx_new_window(g.mlx_ptr, g.map_width * TILE_SIZE,
+        g.map_height * TILE_SIZE, "so_long");
+    if (!g.win_ptr)
+        error_exit("Failed to create a window");
+    load_textures(&g);
+    find_player(&g);
+    render(&g);
+    mlx_hook(g.win_ptr, 2, 1L << 0, key_hook, &g);
+    mlx_hook(g.win_ptr, 17, 0, close_win, &g);
+    mlx_loop(g.mlx_ptr);
+    return (0);
+}
 
